cbbcf5fd99755074c0b6904e850ad971224a825a
Unknown <Griffin.r352@stu.nebo.edu>
2017-10-25 13:08:54 -0600

    Day 6

---
+++ Ghost House/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	public bool ligthOn;
+	Light light;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+		lightOn = true;
+		light.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.F) && lightOn) {
+			lightOn = false;
+			light.enabled = false;
+		}
+
+		else if (Input.GetKeyUp (KeyCode.F) && !lightOn) {
+			lightOn = true;
+			light.enabled = true;
+		}
+	}
+}
